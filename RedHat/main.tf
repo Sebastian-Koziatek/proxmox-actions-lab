dfdeafdd8fb0560a6dde0af83b1e550c2fe4715a
Sebastian Koziatek <seb.koziatek@icloud.com>
2025-12-05 13:08:33 +0100

    Zmieniono tag VM na 'RedHat' oraz aktualizacja main.tf

--- RedHat/main.tf
+++ RedHat/main.tf
@@ -1,21 +1,21 @@
 # Lista wszystkich maszyn RedHat i ich parametrów
 locals {
   all_vms = {
-    1 = { name = "vmrhelsz01", mac = "BC:24:11:65:CB:EA", ip = "10.123.1.101" }
-    2 = { name = "vmrhelsz02", mac = "BC:24:11:FB:9F:51", ip = "10.123.1.102" }
-    3 = { name = "vmrhelsz03", mac = "BC:24:11:BA:D2:5D", ip = "10.123.1.103" }
-    4 = { name = "vmrhelsz04", mac = "BC:24:11:70:19:C3", ip = "10.123.1.104" }
-    5 = { name = "vmrhelsz05", mac = "BC:24:11:02:AD:5D", ip = "10.123.1.105" }
-    6 = { name = "vmrhelsz06", mac = "BC:24:11:8A:CB:50", ip = "10.123.1.106" }
-    7 = { name = "vmrhelsz07", mac = "BC:24:11:8F:78:BB", ip = "10.123.1.107" }
-    8 = { name = "vmrhelsz08", mac = "BC:24:11:CD:23:32", ip = "10.123.1.108" }
-    9 = { name = "vmrhelsz09", mac = "BC:24:11:AA:2F:30", ip = "10.123.1.109" }
-    10 = { name = "vmrhelsz10", mac = "BC:24:11:28:DD:1F", ip = "10.123.1.110" }
-    11 = { name = "vmrhelsz11", mac = "BC:24:11:E5:F6:FB", ip = "10.123.1.111" }
-    12 = { name = "vmrhelsz12", mac = "BC:24:11:73:19:B4", ip = "10.123.1.112" }
-    13 = { name = "vmrhelsz13", mac = "BC:24:11:19:60:D5", ip = "10.123.1.113" }
-    14 = { name = "vmrhelsz14", mac = "BC:24:11:0E:7C:71", ip = "10.123.1.114" }
-    15 = { name = "vmrhelsz15", mac = "BC:24:11:C4:18:59", ip = "10.123.1.115" }
+    1 = { name = "vmrhelsz01", mac = "BC:24:11:65:CB:EA", ip = "10.123.1.1" }
+    2 = { name = "vmrhelsz02", mac = "BC:24:11:FB:9F:51", ip = "10.123.1.2" }
+    3 = { name = "vmrhelsz03", mac = "BC:24:11:BA:D2:5D", ip = "10.123.1.3" }
+    4 = { name = "vmrhelsz04", mac = "BC:24:11:70:19:C3", ip = "10.123.1.4" }
+    5 = { name = "vmrhelsz05", mac = "BC:24:11:02:AD:5D", ip = "10.123.1.5" }
+    6 = { name = "vmrhelsz06", mac = "BC:24:11:8A:CB:50", ip = "10.123.1.6" }
+    7 = { name = "vmrhelsz07", mac = "BC:24:11:8F:78:BB", ip = "10.123.1.7" }
+    8 = { name = "vmrhelsz08", mac = "BC:24:11:CD:23:32", ip = "10.123.1.8" }
+    9 = { name = "vmrhelsz09", mac = "BC:24:11:AA:2F:30", ip = "10.123.1.9" }
+    10 = { name = "vmrhelsz10", mac = "BC:24:11:28:DD:1F", ip = "10.123.1.10" }
+    11 = { name = "vmrhelsz11", mac = "BC:24:11:E5:F6:FB", ip = "10.123.1.11" }
+    12 = { name = "vmrhelsz12", mac = "BC:24:11:73:19:B4", ip = "10.123.1.12" }
+    13 = { name = "vmrhelsz13", mac = "BC:24:11:19:60:D5", ip = "10.123.1.13" }
+    14 = { name = "vmrhelsz14", mac = "BC:24:11:0E:7C:71", ip = "10.123.1.14" }
+    15 = { name = "vmrhelsz15", mac = "BC:24:11:C4:18:59", ip = "10.123.1.15" }
   }
 
   zakres_lista = var.zakres == "all" ? [for i in range(1,16) : i] : distinct(flatten([for part in split(",", var.zakres) : (
